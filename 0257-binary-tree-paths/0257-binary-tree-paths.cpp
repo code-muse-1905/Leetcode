36c3136e8d4a3983831b85e62db28632c4dea0b1
code-muse-1905 <samikshyap2025@gmail.com>
2026-07-08 23:24:59 +0530

    Time: 3 ms (30.12%), Space: 17.9 MB (38.19%) - LeetHub

---
+++ 0257-binary-tree-paths/0257-binary-tree-paths.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<string> ans;
+    void dfs(TreeNode* root, string path) {
+        if (root == nullptr)
+            return;
+        if (path.empty())
+            path = to_string(root->val);
+        else
+            path += "->" + to_string(root->val);
+        if (root->left == nullptr && root->right == nullptr) {
+            ans.push_back(path);
+            return;
+        }
+        dfs(root->left, path);
+        dfs(root->right, path);
+        }
+    vector<string> binaryTreePaths(TreeNode* root) {
+        dfs(root, "");
+        return ans;
+    }
+};
