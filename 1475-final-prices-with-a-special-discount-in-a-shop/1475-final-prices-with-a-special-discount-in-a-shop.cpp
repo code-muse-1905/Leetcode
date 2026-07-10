97433cb25a1c7508476fa9616ed32d8dadb45732
code-muse-1905 <samikshyap2025@gmail.com>
2026-07-10 19:37:12 +0530

    Time: 0 ms (100%), Space: 14.3 MB (29.12%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        stack<int> st;
+        for(int i = prices.size()-1; i >= 0; i--)
+        {
+            int curr = prices[i];
+            while(!st.empty() && st.top() > curr){
+                 st.pop();
+            }
+            if(!st.empty()) prices[i] = curr - st.top();
+            st.push(curr);
+        }
+        return prices;
+    }
+};
