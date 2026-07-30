cd464c260cbd4c8cf6ee1235a15a808a8e095a8e
code-muse-1905 <samikshyap2025@gmail.com>
2026-07-31 00:49:13 +0530

    Time: 3 ms (85.72%), Space: 15.6 MB (44.07%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        unordered_map<int,int> c;
-        c[0]=1;
-        for(int n:nums){
-            unordered_map<int,int> temp;
-            for(auto[t,count]:c){
-                temp[t+n]+=count;
-                 temp[t-n]+=count;
+        int total = accumulate(nums.begin(), nums.end(), 0);
+        if (abs(target) > total) return 0;
+        if ((total + target) % 2) return 0;
+            int sum = (total + target) / 2;
+        int n = nums.size();
+        vector<vector<int>> dp(n + 1, vector<int>(sum + 1, 0));
+        dp[0][0] = 1;
+        for (int i = 1; i <= n; i++)
+            dp[i][0] = 1;
+            for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= sum; j++) {
+              if (nums[i - 1] <= j)
+                    dp[i][j] = dp[i - 1][j] +dp[i - 1][j - nums[i - 1]];
+                else
+                    dp[i][j] = dp[i - 1][j];
             }
-            c=temp;
         }
-        return c[target];
+        return dp[n][sum];
     }
 };
