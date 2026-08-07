17af1ae18b9f42de995ef2746e5ddd1d66b26c53
code-muse-1905 <samikshyap2025@gmail.com>
2026-08-07 23:43:52 +0530

    Time: 0 ms (100%), Space: 10.8 MB (24.34%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
-int solve(vector<int> & nums,int i,vector<int> &dp){
-    if(i>=nums.size()) return 0;
-    if(dp[i]!=-1) return dp[i];
-    int s=nums[i]+solve(nums,i+2,dp);
-    int sk=solve(nums,i+1,dp);
-    return dp[i]=max(s,sk);
-}
-    int rob(vector<int>& nums) {
-     vector<int>dp(nums.size(),-1);
-return solve(nums,0,dp);
+int rob(vector<int> & nums){
+//     if(i>=nums.size()) return 0;
+//     if(dp[i]!=-1) return dp[i];
+//     int s=nums[i]+solve(nums,i+2,dp);
+//     int sk=solve(nums,i+1,dp);
+//     return dp[i]=max(s,sk);
+// }
+//     int rob(vector<int>& nums) {
+//      vector<int>dp(nums.size(),-1);
+// return solve(nums,0,dp);
+  if(nums.size()==1) return nums[0];
+  vector<int> dp(nums.size());
+  dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for (size_t i=2; i<nums.size(); i++) {
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i]);
+        }
+        return dp.back();
     }
 };
