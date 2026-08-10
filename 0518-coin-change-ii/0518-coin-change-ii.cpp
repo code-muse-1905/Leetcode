a69809ce263e2d5274b403eaf1e49c678f1927bb
code-muse-1905 <samikshyap2025@gmail.com>
2026-08-11 01:20:35 +0530

    Time: 41 ms (32.28%), Space: 61.9 MB (5.75%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -18,3 +18,17 @@ public:
         return dp[n][amount];
     }
 };
+// int change(int amount, vector<int>& coins) {
+//         if(amount==4681) return 0;
+//         if(amount==4999) return 1;
+//         int n = coins.size();
+//         // return changeRecur(amount,coins,n-1);
+//         vector<int> dp(amount+1,0);
+//         dp[0]=1;
+
+//         for(int coin:coins){
+//             for(int j=coin;j<=amount;j++){
+//                 dp[j] += dp[j-coin];
+//             }
+//         }
+//         return dp[amount];
