1a17c781972d754013e8f7bed033991a5ca022ca
code-muse-1905 <samikshyap2025@gmail.com>
2026-08-19 01:25:16 +0530

    Time: 23 ms (88.71%), Space: 26.7 MB (73%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -8,11 +8,10 @@ public:
        for(int j=0;j<=m;j++) dp[0][j]=j;
        for(int i=1;i<=n;i++){
         for(int j=1;j<=m;j++){
-            if(str1[i-1]==str2[j-1]){
-                dp[i][j]=1+dp[i-1][j-1];
-            } else{
+            if(str1[i-1]==str2[j-1])
+                dp[i][j]=1+dp[i-1][j-1]; 
+                else
                 dp[i][j]=1+min(dp[i-1][j],dp[i][j-1]);
-            }
         }
        }  
      string ans = "";
