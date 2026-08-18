414b78bd408f8cb8042e6a2ea9bbe62149d1e2e4
code-muse-1905 <samikshyap2025@gmail.com>
2026-08-19 01:24:14 +0530

    Time: 27 ms (36.21%), Space: 26.9 MB (43.31%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -4,12 +4,8 @@ public:
        int n=str1.size();
        int m=str2.size();
        vector<vector<int>>dp(n+1,vector<int>(m+1,0)) ;
-       for(int i=0;i<=n;i++){
-        dp[i][0]=i;
-       }
-       for(int j=0;j<=m;j++){
-        dp[0][j]=j;
-       }
+       for(int i=0;i<=n;i++) dp[i][0]=i;
+       for(int j=0;j<=m;j++) dp[0][j]=j;
        for(int i=1;i<=n;i++){
         for(int j=1;j<=m;j++){
             if(str1[i-1]==str2[j-1]){
@@ -25,15 +21,12 @@ public:
         while(i > 0 && j > 0) {
             if(str1[i-1] == str2[j-1]) {
                 ans += str1[i-1];
-                i--;
-                j--;
+                i--;j--;
             }
             else if(dp[i-1][j] < dp[i][j-1]) {
-                ans += str1[i-1];
-                i--;
+                ans += str1[i-1]; i--;
             } else {
-                ans += str2[j-1];
-                j--;
+                ans += str2[j-1];j--;
             }
         }
         while(i > 0) {
