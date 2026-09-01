be1176ade6a0824de571a8c44a237b2eaeefbb1c
code-muse-1905 <samikshyap2025@gmail.com>
2026-09-01 23:23:01 +0530

    Time: 167 ms (66.79%), Space: 166.1 MB (62.43%) - LeetHub

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -2,34 +2,30 @@ class Solution {
 public:
     vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
         int n = nums.size();
-        vector<int> arr = nums;
-        sort(arr.begin(), arr.end());
-        unordered_map<int, vector<int>> g;
-        unordered_map<int, int> b;
-        unordered_map<int, int> Minindex;
-        int id = 1;
-        g[id].push_back(arr[0]);
-        for (int i = 1; i < n; i++) {
-            if (arr[i] - arr[i - 1] > limit) {
-                id++;
-            }
-            g[id].push_back(arr[i]);
-        }
-        id = 1;
+        vector<pair<int, int>> sorted;
         for (int i = 0; i < n; i++) {
-        if (i > 0 && arr[i] - arr[i - 1] > limit) {
-                id++;
+            sorted.push_back({nums[i], i});
+        }
+        sort(sorted.begin(), sorted.end());
+        vector<int> a = nums;
+        int start = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i == n || sorted[i].first - sorted[i - 1].first > limit) {
+                vector<int> values;
+                vector<int> indices;
+
+                for (int j = start; j < i; j++) {
+                    values.push_back(sorted[j].first);
+                    indices.push_back(sorted[j].second);
+                }
+                sort(indices.begin(), indices.end());
+                for (int j = 0; j < values.size(); j++) {
+                    a[indices[j]] = values[j];
+                }
+
+                start = i;
             }
-        b[arr[i]] = id;
         }
-        for (int i = 1; i <= id; i++) {
-            Minindex[i] = 0;
-        }
-        for (int i = 0; i < n; i++) {
-            int groupId = b[nums[i]];
-            nums[i] = g[groupId][Minindex[groupId]];
-            Minindex[groupId]++;
-        }
-        return nums;
+        return a;
     }
 };
